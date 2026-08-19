f0d90f3d7ab355af53594022c3e8d90942b5c04b
letallemartin <letallemartin@gmail.com>
2026-08-19 11:31:30 +0200

    code fonctionnel

--- rush00/rush00.c
+++ rush00/rush00.c
@@ -9,8 +9,46 @@
 /*                                                    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <unistd.h> 
+#include <stdio.h>
+#include "ft_putchar.c"
 void    rush(int x, int y)
 {
-    
+
+    int     i; 
+    int     j;
+    i = 0; // 0 -> y 
+
+    if (x > 0 && y > 0) 
+    {
+        while (i < y)
+        {
+            j = 0;
+            while (j < x)
+            {
+                if (i == 0 && j == 0|| j == 0 && i == y - 1 || j == x - 1 && i == 0 || j == x - 1 && i == y - 1)
+                {
+                    ft_putchar('0'); //if dans un coin -> ft_putchar('0')
+                }
+
+                else if ((i == 0  || i == y - 1) && 0 < j < x - 1 )
+                {
+                    ft_putchar('-'); //if bord haut ou ligne bas -> ft_putchar('-')
+                }
+
+                else if ((j == 0  || j == x - 1) && 0 < i < y - 1 )
+                {
+                    ft_putchar('|'); //if ligne bord droit ou bord gauche -> ft_putchar('|')
+                }
+
+                else
+                {
+                    ft_putchar(' '); // else if centre ft_putchar(' ')
+                }
+                j++;
+            }
+            write(1,"\n",1);
+            i++;
+        }
+    }
 }
