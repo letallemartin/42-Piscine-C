552a3705348d88e6e5f57d405db75469a834efd7
letallemartin <letallemartin@gmail.com>
2026-08-20 13:35:18 +0200

    ft_str_is_printable.c

---
+++ cell02/ex06/ft_str_is_printable.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 15:00:00 by letalle           #+#    #+#             */
+/*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_str_is_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 32 || str[i] > 126)
+		{
+			printf("error");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+// int main(void)
+// {
+// 	char str[] = "Hello";
+// 	ft_str_is_printable(str);
+// 	return 0;
+// }
