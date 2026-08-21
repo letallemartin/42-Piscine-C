7a01aff262e133b188e4dbc94e97a57ef0be99e8
letallemartin <letallemartin@gmail.com>
2026-08-21 16:43:45 +0200

    ft_strcapitalize.c

--- cell02/ex09/ft_strcapitalize.c
+++ cell02/ex09/ft_strcapitalize.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcapitalize.c                                     :+:      :+:    :+:   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -9,31 +9,34 @@
 /*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
 
 	i = 0;
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] -= 32;
+	}
 	while (str[i] != '\0')
 	{
-		if ((str[i] < 'A' || 'Z' < str[i]) && (str[i] < 'a' || str[i] > 'z') && (str[i] < 48 || str[i] > 57))
+		if ((str[i] < 'A' || 'Z' < str[i])
+			&& (str[i] < 'a' || str[i] > 'z')
+			&& (str[i] < 48 || str[i] > 57))
 		{
-			if (str[i++] >= 'a' && str[i++] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				str[i++] -= 32;
+				str[i + 1] -= 32;
 			}
 		}
-		printf("%c", str[i]);
 		i++;
 	}
 	return (str);
 }
 
-int main(void)
-{
-	char str[] = "moi je suis+moi que toi5moi";
-	ft_strcapitalize(str);
-	return 0;
-}
-
+// int main(void)
+// {
+// 	char str[] = "moi je suis+moi que toi5moi";
+// 	ft_strcapitalize(str);
+// 	return 0;
+// }
