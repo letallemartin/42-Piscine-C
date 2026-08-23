83d34f3ecbfe3d0a2fe3a7b60e9f9f761d01c321
letallemartin <letallemartin@gmail.com>
2026-08-23 13:31:57 +0200

    ft_strcat.c 1/2

---
+++ cell03/ex02/ft_strcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 15:00:00 by letalle           #+#    #+#             */
+/*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+int	ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int j;
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		printf("%c", dest[i]);
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		printf("%c", dest[i]);
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (0);
+}
+
+int main(void)
+{
+	char str1[] = "texte_long";
+	char str2[] = "texte_court";
+	ft_strcat(str2, str1);
+	return 0;
+}
