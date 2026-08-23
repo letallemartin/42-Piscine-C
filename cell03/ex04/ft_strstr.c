74b3ebc0f75da83857bf7bc2cadabd5e4050c8cd
letallemartin <letallemartin@gmail.com>
2026-08-23 20:26:53 +0200

    ft_strstr.c finish

--- cell03/ex04/ft_strstr.c
+++ cell03/ex04/ft_strstr.c
@@ -9,39 +9,35 @@
 /*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	ft_strstr(char *str, char *to_find)
+char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	j;
 
 	i = 0;
 	j = 0;
-	if (to_find[0] == '\0' )
-	{
-		return (haystack);
-	}
+	if (to_find[0] == '\0')
+		return (str);
 	while (str[i] != '\0')
 	{
-		if (str[i] == to_find[j])
+		while (str[i + j] != '\0' && str[i + j] == to_find[j])
 		{
-			while (str[i + j] != '\0')
-			{
-				if (to_find[j] ==  '\0')
-				{
-					return &str[i]
-				}
-				j++;
-			}
+			printf("i = %d\n", i);
+			if (to_find[j + 1] == '\0')
+				return (&str[i]);
+			j++;
 		}
+		j = 0;
 		i++;
 	}
 	return (NULL);
 }
 
-int main(void)
-{
-	char str[] = "piscine";
-	char search[] = "sc";
-	ft_strstr(str, search);
-	return 0;
-}
+// int main(void)
+// {
+// 	char str[] = "aaaaaab";
+// 	char search[] = "aab";
+// 	printf("%p",ft_strstr(str, search));
+// 	return 0;
+// }
+// printf("i = %d\n", i);
