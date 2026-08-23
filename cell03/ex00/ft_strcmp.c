031d56febcb1044676e2d019443ec070e4b05325
letallemartin <letallemartin@gmail.com>
2026-08-23 11:36:21 +0200

    ft_strcmp.c

---
+++ cell03/ex00/ft_strcmp.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 15:00:00 by letalle           #+#    #+#             */
+/*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
+// int main(void)
+// {
+// 	char str1[] = "abcdef";
+// 	char str2[] = "abc";
+// 	ft_strcmp(str1, str2);
+// 	return 0;
+// }
