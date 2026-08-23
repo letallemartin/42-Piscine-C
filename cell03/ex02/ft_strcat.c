9d04749b921e4b447a0f984baf9008890f251c3b
letallemartin <letallemartin@gmail.com>
2026-08-23 13:41:52 +0200

    ft_strcat.c finish

--- cell03/ex02/ft_strcat.c
+++ cell03/ex02/ft_strcat.c
@@ -9,22 +9,20 @@
 /*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
 int	ft_strcat(char *dest, char *src)
 {
 	int	i;
-	int j;
+	int	j;
+
 	i = 0;
 	j = 0;
 	while (dest[i] != '\0')
 	{
-		printf("%c", dest[i]);
 		i++;
 	}
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-		printf("%c", dest[i]);
 		i++;
 		j++;
 	}
@@ -32,10 +30,11 @@ int	ft_strcat(char *dest, char *src)
 	return (0);
 }
 
-int main(void)
-{
-	char str1[] = "texte_long";
-	char str2[] = "texte_court";
-	ft_strcat(str2, str1);
-	return 0;
-}
+// int main(void)
+// {
+// 	char str1[] = "texte_long";
+// 	char str2[] = "texte_court";
+// 	ft_strcat(str2, str1);
+// 	return 0;
+// }
+// printf("%c", dest[i]);
