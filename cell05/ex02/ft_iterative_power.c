63d1353613f113963a029b221e91670e56b3cc15
letallemartin <letallemartin@gmail.com>
2026-09-05 16:49:35 +0200

    ft_iterative_power.c

---
+++ cell05/ex02/ft_iterative_power.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 15:00:00 by letalle           #+#    #+#             */
+/*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+int	ft_iterative_power(int nb, int power)
+{
+	int res;
+
+	res = 1;
+	if (power < 0)
+	{
+		return (0);
+	}
+	else if (power == 0 && nb == 0)
+	{
+		return (1);
+	}
+	while (power > 0)
+	{
+		res = res * nb;
+		power -= 1;
+	}
+	return (res);
+}
+
+int main(void)
+{
+	printf("%d\n", ft_iterative_power(4, 3));
+	return 0;
+}
