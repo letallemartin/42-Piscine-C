cb8cc6b80b20468e6e7c92825953634ce45638ab
letallemartin <letallemartin@gmail.com>
2026-09-06 12:24:29 +0200

    norminette

--- cell00/ex05/ft_print_comb.c
+++ cell00/ex05/ft_print_comb.c
@@ -1,51 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*  ft_print_comb.c                                   :+:      :+:    :+:   */
+/*   ft_print_comb.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*                                                  +#+  +:+       +#+        */
+/*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*                                                     #+#    #+#             */
-/*                                                    ###   ########.fr       */
+/*   Created: 2026/08/19 15:00:00 by letalle           #+#    #+#             */
+/*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-#include <unistd.h> 
-
-void ft_print_comb(void)
+void	ft_print_comb(void)
 {
-    int a;
-    int b;
-    int c;
-    char tab[3];
-    a = 0;
-    while (a < 8)
-    {
-        b = a + 1;
-        while (b < 9)
-        {
-            c = b + 1;
-            while (c < 10)
-            {
-                if (a < b && b < c)
-                {
-                    tab[0] = a + '0';
-                    tab[1] = b + '0';
-                    tab[2] = c + '0';
-                    write(STDOUT_FILENO, tab , 3);
-                    if (a != 7)
-                    {
-                        write(STDOUT_FILENO, ", " , 2);
-                    }
-                }
-                c++;
-            }
-            b++;
-        }
-        a++;
-    }
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	while (a < '8')
+	{
+		b = a + 1;
+		while (b < '9')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				write(STDOUT_FILENO, &a, 1);
+				write(STDOUT_FILENO, &b, 1);
+				write(STDOUT_FILENO, &c, 1);
+				if (a != 7)
+					write(STDOUT_FILENO, ", ", 2);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
 }
 
-// void main(void) {
-//     ft_print_comb();
-// }
+void main(void) {
+    ft_print_comb();
+}
