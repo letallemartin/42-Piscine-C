38b0b17737118d2aaa964c013da804da259c507b
letallemartin <letallemartin@gmail.com>
2026-09-11 18:32:13 +0200

    debut ft_print_params.c

---
+++ cell06/ex01/ft_print_params.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 15:00:00 by letalle           #+#    #+#             */
+/*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char argv[])
+{
+	int j;
+
+	j = 1;
+	while (argv[j] != NULL)
+	{
+		ft_putstr(argv[j]);
+		j++;
+	}
+	return (0);
+}
