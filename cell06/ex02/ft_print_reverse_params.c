924d8f24697e70eb3f3d4b2fbdb1191e9f10ead8
letallemartin <letallemartin@gmail.com>
2026-09-12 16:44:37 +0200

    ft_print_reverse_params.c

---
+++ cell06/ex02/ft_print_reverse_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_params.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 15:00:00 by letalle           #+#    #+#             */
+/*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int	j;
+
+	j = 0;
+	while (argv[j + 1] != NULL)
+	{
+		j++;
+	}
+	while (j > 0)
+	{
+		ft_putstr(argv[j]);
+		write(1, "\n", 2);
+		j--;
+	}
+	return (0);
+}
