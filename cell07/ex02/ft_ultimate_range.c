0bae189a40e01d5998878477d7a3302d96738aeb
letallemartin <letallemartin@gmail.com>
2026-09-19 16:46:25 +0200

    ft_ultimate_range.c 1/2

---
+++ cell07/ex02/ft_ultimate_range.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 15:00:00 by letalle           #+#    #+#             */
+/*   Updated: 2026/08/19 15:00:00 by letalle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+int	ft_range(int **range, int min, int max)
+{
+	int size;
+	int	i;
+
+	size = max - min;
+	i = 0;
+	if (min >= max)
+	{
+		*range = NULL
+		return (0);
+	}
+	*range = malloc((max - min) * sizeof(int));
+	while (min < max)
+	{
+		tab[i] = min;
+		min++;
+		i++;
+	}
+	return (size);
+}
