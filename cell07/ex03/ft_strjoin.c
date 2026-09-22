b5d738d6981a64d5020af6d24e3d09671b84ed59
letallemartin <letallemartin@gmail.com>
2026-09-22 21:03:13 +0200

    advance ex03 cell07

--- cell07/ex03/ft_strjoin.c
+++ cell07/ex03/ft_strjoin.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: letalle <letalle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,50 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
-
-char	*ft_strjoin(int size, char **strs, char *sep)
-{
-	int		i;
-	int		j;
-	int		full_size;
-	char	*str2;
-
-	i = 0;
-	j = 0;
-	full_size = calc_size(size, **strs, *sep);
-	
-	str2 = malloc(full_size * sizeof(char));
-	i = 0;
-	while (i < size)
-	{
-		while (strs[i][j] != '\n')
-		{
-			str2[i] = strs[i][j]
-			j++;
-		}
-		i++;
-	}
-}
-
-int	calc_size(int	size, char **strs, char *sep)
-{
-	int	i;
-	int	full_size;
-
-	i = 0;
-	full_size = 0;
-	if (size <= 0)
-		return (str2);
-	while (i < size)
-	{
-		full_size += ft_strlen(str[i])
-		i++;
-		if (i < size - 1)
-			full_size += ft_strlen(sep)
-	}
-	full_size += 1;
-	return (size);
-}
+#include <unistd.h>
 
 int	ft_strlen(char *str)
 {
@@ -67,10 +24,60 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
+int	calc_size(int size, char **strs, char *sep)
+{
+	int	i;
+	int	full_size;
+
+	i = 0;
+	full_size = 0;
+	if (size <= 0)
+		return (size);
+	while (i < size)
+	{
+		full_size += ft_strlen(strs[i]);
+		i++;
+		if (i < size - 1)
+			full_size += ft_strlen(sep);
+		
+	}
+	full_size += 1;
+	return (full_size);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int		i;
+	int		j;
+	int		full_size;
+	char	*str2;
+	int g = 0;
+	i = 0;
+	full_size = calc_size(size, strs, sep);
+	
+	str2 = malloc(full_size * sizeof(char));
+	while (i < size)
+	{
+		j = 0;
+		while (strs[i][j] != '\0')
+		{
+			str2[g] = strs[i][j];
+			write(1,&strs[i][j],1);
+			g++;
+			j++;
+		}
+		i++;
+	}
+	str2[g] = '\0';
+	int p = ft_strlen(str2);
+	write(1,str2,i);
+	return (str2);
+}
+
 int	main(void)
 {
-	**str = ["hello", "its", "me"]
-	*sep = ", "
-	*ft_strjoin(3, **strs, *sep)
+	char *strs[] = {"hel", "its"};
+	char *sep = ", ";
+	char *str2 = ft_strjoin(2, strs, sep);
 	return (0);
 }
